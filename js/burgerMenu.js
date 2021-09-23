e014845c3df0d1921bc8fb00dacc7cbdfd8711b7
Alexandr <net_box@ukr.net>
2021-09-23 23:39:09 +0300

    fix: при натисканні на якір в бургер-меню, список закривається

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,9 +1,18 @@
 const burger = document.querySelector('.humburger-menu');
 const menuMob = document.querySelector('.menu');
+const menuList = document.querySelector('.menu-list');
 
 const toggleMenu = () => {
-    menuMob.classList.toggle('menu-active');
     burger.classList.toggle('humburger-menu-active');
+    menuMob.classList.toggle('menu-active');
+
+    menuList.addEventListener('click', (event) =>{
+        if (event.target.classList.contains('menu-list__link')){
+            burger.classList.remove('humburger-menu-active');
+            menuMob.classList.remove('menu-active');
+        };
+    })
 }
 
-burger.addEventListener('click', () => toggleMenu());
+burger.addEventListener('click', () => toggleMenu()); // варіант -1
+//burger.addEventListener('click', toggleMenu); -> варіант - 2
